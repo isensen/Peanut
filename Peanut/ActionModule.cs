a8d74313f893f5d7e45e2ca69b4f0cd0cbb9193e
Henryfan <henryfan@msn.com>
2012-11-12 12:14:12 +0000

    on PostAuthorizeRequest event invoke action  event

--- Peanut/ActionModule.cs
+++ Peanut/ActionModule.cs
@@ -15,39 +15,44 @@ namespace Peanut
 		{
 			context.BeginRequest += new EventHandler(this.OnRequest);
 			context.EndRequest += new EventHandler(this.OnEndRequest);
+            context.PostAuthorizeRequest += OnPostAuthorizeRequest;
 		}
-		private void OnRequest(object sender, EventArgs e)
-		{
+        private void OnPostAuthorizeRequest(object sender, EventArgs e)
+        {
             WebContext context = WebContext.Current;
             try
-			{
-				string filePath = WebContext.Current.Request.FilePath;
+            {
+                string filePath = WebContext.Current.Request.FilePath;
                 //判断当前类型是否需要进行模板处理,默认只对aspx进行处理
-				if (Utils.IsHandler(Path.GetExtension(filePath)))
-				{
-                    
+                if (Utils.IsHandler(Path.GetExtension(filePath)))
+                {
+
                     string url = filePath.Substring(0, filePath.LastIndexOf('.'));
                     //获取对应的控制器方法
-					ActionItem action = Utils.GetAction(url);
+                    ActionItem action = Utils.GetAction(url);
                     context.ActionPath = url;
                     context.Action = action;
                     context.RequestType = (RequestType)Enum.Parse(typeof(RequestType), context.Request.RequestType);
-					if (action != null)
-					{
-						object obj = action.Execute();
+                    if (action != null)
+                    {
+                        object obj = action.Execute();
                         context.Result = obj;
                         //如果是一个返回类型
-						if (obj != null && obj is IResult)
-						{
-							((IResult)obj).Execute();
-						}
-					}
-				}
-			}
-			catch (Exception appError)
-			{
+                        if (obj != null && obj is IResult)
+                        {
+                            ((IResult)obj).Execute();
+                        }
+                    }
+                }
+            }
+            catch (Exception appError)
+            {
                 context.AppError = appError;
-			}
+            }
+        }
+		private void OnRequest(object sender, EventArgs e)
+		{
+            
 		}
 		private void OnEndRequest(object sender, EventArgs e)
 		{
